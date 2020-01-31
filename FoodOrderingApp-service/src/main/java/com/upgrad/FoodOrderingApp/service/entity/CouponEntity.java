ef8bbc6df901b323b58b010c6e13ee2e36b2b48b
amitborkar98 <amitborkar98@gmail.com>
2020-01-31 20:48:12 +0530

    Created Coupon entity

---
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/entity/CouponEntity.java
@@ -0,0 +1,57 @@
+package com.upgrad.FoodOrderingApp.service.entity;
+
+import javax.persistence.*;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "coupon")
+public class CouponEntity {
+
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @Column(name = "uuid")
+    @Size(max = 200)
+    private  String uuid;
+
+    @Column(name = "coupon_name")
+    @Size(max = 30)
+    private String coupon_name;
+
+    @Column(name = "percent")
+    private Integer percent;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(String uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getCoupon_name() {
+        return coupon_name;
+    }
+
+    public void setCoupon_name(String coupon_name) {
+        this.coupon_name = coupon_name;
+    }
+
+    public Integer getPercent() {
+        return percent;
+    }
+
+    public void setPercent(Integer percent) {
+        this.percent = percent;
+    }
+}
