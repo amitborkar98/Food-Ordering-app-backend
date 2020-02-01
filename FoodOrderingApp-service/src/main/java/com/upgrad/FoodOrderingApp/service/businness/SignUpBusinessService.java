7b8bb2b59474bdd43b88b63c1199e4a5b713ba34
amitborkar98 <amitborkar98@gmail.com>
2020-02-01 13:42:25 +0530

    Added e-mail validation for SignUp

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/SignUpBusinessService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/SignUpBusinessService.java
@@ -20,12 +20,29 @@ public class SignUpBusinessService {
     @Transactional(propagation = Propagation.REQUIRED)
     public CustomerEntity signupCustomer(CustomerEntity customerEntity)throws SignUpRestrictedException {
 
+        String email = customerEntity.getEmail();
+        int a_count = 0;
+        int charCount = 0;
+        int dot_count = 0;
+        for (int i = 0; i < email.length(); i++) {
+            char ch = email.charAt(i);
+            ch = Character.toUpperCase(ch);
+            //to check if the password contains a digit or alphabet
+            if ((ch >= '0' && ch <= '9') || (ch >= 'A' && ch <= 'Z')) charCount++;
+            else if (ch == '.') dot_count++;
+            else if (ch == '@') a_count++;
+        }
+
         if (customerDao.getCustomerByContact(customerEntity.getContact_number()) != null) {
             throw new SignUpRestrictedException("SGR-001", "This contact number is already registered! Try other contact number.");
         }
-        else if(customerEntity.getFirstname() == null || customerEntity.getContact_number() == null || customerEntity.getEmail() == null || customerEntity.getPassword() == null) {
+        else if (customerEntity.getFirstname() == null || customerEntity.getContact_number() == null || customerEntity.getEmail() == null || customerEntity.getPassword() == null) {
             throw new SignUpRestrictedException("(SGR-005", "Except last name all fields should be filled");
         }
+        else if (charCount < 1 || dot_count < 1 || a_count <1){
+            throw new SignUpRestrictedException("(SGR-002", "Invalid email-id format!");
+        }
+
         else {
             String[] encryptedText = passwordCryptographyProvider.encrypt(customerEntity.getPassword());
             customerEntity.setSalt(encryptedText[0]);
