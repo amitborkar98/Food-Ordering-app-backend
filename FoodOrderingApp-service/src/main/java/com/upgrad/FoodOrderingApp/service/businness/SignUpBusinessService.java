87b4a141ef368f790c19c5bb62f2ce5354dc4742
amitborkar98 <amitborkar98@gmail.com>
2020-02-01 13:58:28 +0530

    Added contact number validation

--- FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/SignUpBusinessService.java
+++ FoodOrderingApp-service/src/main/java/com/upgrad/FoodOrderingApp/service/businness/SignUpBusinessService.java
@@ -33,16 +33,27 @@ public class SignUpBusinessService {
             else if (ch == '@') a_count++;
         }
 
+        String contact = customerEntity.getContact_number();
+        int not_number_count = 0;
+        for (int i = 0; i < contact.length(); i++) {
+            char ch = contact.charAt(i);
+            ch = Character.toUpperCase(ch);
+            //to check if the password contains a alphabet
+            if ((ch >= 'A' && ch <= 'Z')) not_number_count++;
+        }
+
         if (customerDao.getCustomerByContact(customerEntity.getContact_number()) != null) {
             throw new SignUpRestrictedException("SGR-001", "This contact number is already registered! Try other contact number.");
         }
         else if (customerEntity.getFirstname() == null || customerEntity.getContact_number() == null || customerEntity.getEmail() == null || customerEntity.getPassword() == null) {
             throw new SignUpRestrictedException("(SGR-005", "Except last name all fields should be filled");
         }
-        else if (charCount < 1 || dot_count < 1 || a_count <1){
+        else if (charCount < 1 || dot_count < 1 || a_count < 1){
             throw new SignUpRestrictedException("(SGR-002", "Invalid email-id format!");
         }
-
+        else if(not_number_count > 0 || customerEntity.getContact_number().length() != 9){
+            throw new SignUpRestrictedException("(SGR-003", "Invalid contact number!");
+        }
         else {
             String[] encryptedText = passwordCryptographyProvider.encrypt(customerEntity.getPassword());
             customerEntity.setSalt(encryptedText[0]);
